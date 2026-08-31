ce0ab521edd92b3b96677655d9c4118817d52a4e
jdy7149 <184757480+jdy7149@users.noreply.github.com>
2026-08-31 23:03:14 +0900

    Modify structure of html

--- src/components/EduExperienceForm.jsx
+++ src/components/EduExperienceForm.jsx
@@ -4,36 +4,36 @@ function EduExperienceForm({ isSubmitted, onSubmit, onEdit }) {
   const [eduInfo, setEduInfo] = useState([]);
 
   return (
-    <form action="">
-      <div>
-        <h1>Education Experience</h1>
-        {eduInfo.map(({ school, course, eduStartDate, eduEndDate }) => {
-          <div>
-            <label htmlFor="school">Shcool</label>
-            <input type="text" name="school" id="school" value={school} required readOnly={isSubmitted}/>
-            <label htmlFor="course">Course</label>
-            <input type="text" name="course" id="course" value={course} required readOnly={isSubmitted} />
-            <label htmlFor="eduStart">Start Date</label>
-            <input type="date" name="eduStart" id="eduStart" value={eduStartDate} required readOnly={isSubmitted} />
-            <label htmlFor="eduEnd">End Date</label>
-            <input type="date" name="eduEnd" id="eduEnd" value={eduEndDate} readOnly={isSubmitted} />
+    <div id="edu">
+      <h1>Education Experience</h1>
+      {eduInfo.map(({ school, course, eduStartDate, eduEndDate }, i) => {
+        <div>
+          <form className="edu-form">
+            <label htmlFor={'school-' + i}>Shcool</label>
+            <input type="text" name="school" id={'school-' + i} value={school} required readOnly={isSubmitted}/>
+            <label htmlFor={'course-' + i}>Course</label>
+            <input type="text" name="course" id={'course-' + i} value={course} required readOnly={isSubmitted} />
+            <label htmlFor={'eduStart-' + i}>Start Date</label>
+            <input type="date" name="eduStart" id={'eduStart-' + i} value={eduStartDate} required readOnly={isSubmitted} />
+            <label htmlFor={'eduEnd-' + i}>End Date</label>
+            <input type="date" name="eduEnd" id={'eduEnd-' + i} value={eduEndDate} readOnly={isSubmitted} />
             <div>
               {!isSubmitted && <button type="button">Delete</button>} 
             </div>
-          </div>
-        })}
-        <div>
-          {isSubmitted ? (
-            <button type="button" onClick={onEdit}>Edit</button>
-          ) : (
-            <>
-              <button type="button" onClick={setEduInfo([...eduInfo, {id:crypto.randomUUID()}])}>Add</button>
-              <button type="submit" onClick={onSubmit}>Submit</button>
-            </>
-          )}
+          </form>
         </div>
+      })}
+      <div>
+        {isSubmitted ? (
+          <button type="button" onClick={onEdit}>Edit</button>
+        ) : (
+          <>
+            <button type="button" onClick={setEduInfo([...eduInfo, {id:crypto.randomUUID()}])}>Add</button>
+            <button type="submit" onClick={onSubmit}>Submit</button>
+          </>
+        )}
       </div>
-    </form>
+    </div>
   )
 }
 
--- src/components/WorkExperienceForm.jsx
+++ src/components/WorkExperienceForm.jsx
@@ -4,36 +4,36 @@ function WorkExperienceForm({ isSubmitted, onSubmit, onEdit }) {
   const [workInfo, setWorkInfo] = useState([]);
 
   return (
-    <form action="">
-      <div>
-        <h1>Work Experience</h1>
-        {workInfo.map(({ company, position, responsibilities, workStartDate, workEndDate }) => {
-          <div>
-            <label htmlFor="company">Company</label>
-            <input type="text" name="company" id="company" value={company}/>
-            <label htmlFor="position">Position</label>
-            <input type="text" name="position" id="position" value={position} />
-            <label htmlFor="resposibilities">Resposibilities</label>
-            <input type="text" name="resposibilities" id="resposibilities" value={responsibilities} />
-            <label htmlFor="workStart">Start Date</label>
-            <input type="date" name="workStart" id="workStart" value={workStartDate} />
-            <label htmlFor="workEnd">End Date</label>
-            <input type="date" name="workEnd" id="workEnd" value={workEndDate} />
-            <button type="button">Delete</button>
-          </div>
-        })}
+    <div id="work">
+      <h1>Work Experience</h1>
+      {workInfo.map(({ company, position, responsibilities, workStartDate, workEndDate }, i) => {
         <div>
-          {isSubmitted ? (
-            <button type="button" onClick={onEdit}>Edit</button>
-          ) : (
-            <>
-              <button type="button" onClick={setWorkInfo([...workInfo, {id: crypto.randomUUID}])}>Add</button>
-              <button type="submit" onClick={onSubmit}>Submit</button>
-            </>
-          )}
+          <form className="work-form">
+            <label htmlFor={'company-' + i}>Company</label>
+            <input type="text" name="company" id={'company-' + i} value={company}/>
+            <label htmlFor={'position-' + i}>Position</label>
+            <input type="text" name="position" id={'position-' + i} value={position} />
+            <label htmlFor={'resposibilities-' + i}>Resposibilities</label>
+            <input type="text" name="resposibilities" id={'resposibilities-' + i} value={responsibilities} />
+            <label htmlFor={'workStart-' + i}>Start Date</label>
+            <input type="date" name="workStart" id={'workStart-' + i} value={workStartDate} />
+            <label htmlFor={'workEnd-' + i}>End Date</label>
+            <input type="date" name="workEnd" id={'workEnd-' + i} value={workEndDate} />
+            <button type="button">Delete</button>
+          </form>
         </div>
+      })}
+      <div>
+        {isSubmitted ? (
+          <button type="button" onClick={onEdit}>Edit</button>
+        ) : (
+          <>
+            <button type="button" onClick={setWorkInfo([...workInfo, {id: crypto.randomUUID}])}>Add</button>
+            <button type="submit" onClick={onSubmit}>Submit</button>
+          </>
+        )}
       </div>
-    </form>
+    </div>
   )
 }
 
