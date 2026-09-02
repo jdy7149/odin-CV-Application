41ac7fbcdabf8cb9ee11925794095df069d3c79b
jdy7149 <184757480+jdy7149@users.noreply.github.com>
2026-09-02 22:43:14 +0900

    Lift data state to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,68 @@
-import GeneralInfoForm from "./components/GeneralInfoForm"
-import EduExperienceForm from "./components/EduExperienceForm"
-import WorkExperienceForm from "./components/WorkExperienceForm"
-import GeneralInfo from "./components/GeneralInfo"
-import EduExperience from "./components/EduExperience"
-import WorkExperience from "./components/WorkExperience"
-import { useState } from "react"
+import GeneralInfoForm from "./components/GeneralInfoForm";
+import EduExperienceForm from "./components/EduExperienceForm";
+import WorkExperienceForm from "./components/WorkExperienceForm";
+import GeneralInfo from "./components/GeneralInfo";
+import EduExperience from "./components/EduExperience";
+import WorkExperience from "./components/WorkExperience";
+
+import parseFormData from "./utils/parseFormData";
+
+import { useState } from "react";
 
 function App() {
   const [isGeneralSubmitted, setIsGeneralSubmitted] = useState(false);
   const [isEduSubmitted, setIsEduSubmitted] = useState(false);
   const [isWorkSubmitted, setIsWorkSubmitted] = useState(false);
 
-  let generalInfo = {};
-  let eduItems = [];
-  let workItems = [];
-
-  // Parse data of input tags' value in 'className' forms within 'idName' div
-  function parseFormData(idName, className) {
-    return Array.from(document.querySelectorAll(`#${idName} .${className}`))
-      .map((formElem) => Object.entries(new FormData(formElem).entries()));
-  }
+  const [generalInfo, setGeneralInfo] = useState({});
+  const [eduItems, setEduItems] = useState([]);
+  const [workItems, setWorkItems] = useState([]);
 
   function onGeneralSubmit() {
-    generalInfo = new FormData(document.querySelector('#general'));
-
+    setGeneralInfo(
+      Object.fromEntries(
+        new FormData(document.querySelector("#general")).entries(),
+      ),
+    );
     setIsGeneralSubmitted(true);
   }
 
   function onEduSubmit() {
-    eduItems = parseFormData('edu', 'edu-sector');
-
+    setEduItems(parseFormData("edu", "edu-form"));
     setIsEduSubmitted(true);
   }
 
   function onWorkSubmit() {
-    workItems = parseFormData('work', 'work-sector');
-
+    setWorkItems(parseFormData("work", "work-form"));
     setIsWorkSubmitted(true);
   }
 
   return (
     <>
-    <div>
-      <h1>CV-Applicatioon</h1>
-    </div>
-    <div>
-      <GeneralInfoForm info={} isSubmitted={isGeneralSubmitted} onSubmit={onGeneralSubmit}/>
-      <EduExperienceForm isSubmitted={isEduSubmitted} onSubmit={onEduSubmit}/>
-      <WorkExperienceForm isSubmitted={isWorkSubmitted} onSubmit={onWorkSubmit} />
-    </div>
-    <div>
-      <GeneralInfo info={generalInfo} />
-      <EduExperience items={eduItems} />
-      <WorkExperience items={workItems} />
-    </div>
+      <div>
+        <h1>CV-Applicatioon</h1>
+      </div>
+      <div>
+        <GeneralInfoForm
+          isSubmitted={isGeneralSubmitted}
+          onSubmit={onGeneralSubmit}
+        />
+        <EduExperienceForm
+          isSubmitted={isEduSubmitted}
+          onSubmit={onEduSubmit}
+        />
+        <WorkExperienceForm
+          isSubmitted={isWorkSubmitted}
+          onSubmit={onWorkSubmit}
+        />
+      </div>
+      <div>
+        <GeneralInfo info={generalInfo} />
+        <EduExperience items={eduItems} />
+        <WorkExperience items={workItems} />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
