fb7fe8bf0402140135411e0da165fc7328c50ae8
jdy7149 <184757480+jdy7149@users.noreply.github.com>
2026-09-05 00:42:21 +0900

    Remove adding uuid to generated object

--- src/utils/parseFormData.js
+++ src/utils/parseFormData.js
@@ -1,11 +1,8 @@
 // Parse data of input tags' value in 'className' forms within 'idName' div
 function parseFormData(idName, className) {
-  return Array.from(
-    document.querySelectorAll(`#${idName} .${className}`)
-  ).map((formElem) => ({
-    id: crypto.randomUUID(),
-    ...Object.fromEntries(new FormData(formElem).entries()),
-  }));
+  return Array.from(document.querySelectorAll(`#${idName} .${className}`)).map(
+    (formElem) => Object.fromEntries(new FormData(formElem).entries()),
+  );
 }
 
 export default parseFormData;
